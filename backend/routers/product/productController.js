baed897ae81d9d5c59aad01846d31f37c135734b
Yaswanth <yaswanthravula0@gmail.com>
2023-01-04 23:57:02 +0530

    Updated

---
+++ backend/routers/product/productController.js
@@ -0,0 +1,7 @@
+const mysql=require("mysql2");
+const express=require("express");
+const router=express.Router();
+const sqlConnection=require("../../mySqlConncection");
+router.post("/",async(req,res,err)=>{
+   
+})
